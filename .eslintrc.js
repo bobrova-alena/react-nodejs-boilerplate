67d35a571975060706b5f8679a3165fc18e204ee
LAPTOP-ND5NS0AC\Alena <bobrova555@gmail.com>
2021-06-08 17:17:01 +0300

    refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 const getPathInternal = pattern => ({ pattern, group: 'internal' });
 
-const order = {
+const importOrder = {
   pathGroups: [getPathInternal('~**'), getPathInternal('~**/**')],
   groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
   alphabetize: { order: 'asc' },
@@ -8,44 +8,37 @@ const order = {
 };
 
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module', // Allows for the use of imports
-        project: './tsconfig.json',
-        ecmaFeatures: {
-          jsx: true, // Allows for the parsing of JSX
-        },
-        warnOnUnsupportedTypeScriptVersion: true,
-    },
-    ignorePatterns: ['/dist', "/webpack/*.js", ".eslintrc.js"],
-    extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-      'plugin:prettier/recommended'
-    ],
-    plugins: ['import','react-hooks'],
-    rules: {
-      indent: ['warn', 2, { SwitchCase: 1 }],
-      'no-multi-spaces': ['warn'],
-    },
-    overrides: [{
-      files: ['*.ts', '*.tsx', '*.js',],
-      rules: {
-        "prettier/prettier": ["warn"],
-        'import/order': ['error', order],
-        'import/no-duplicates': 'error',
-        'curly': ['error', 'all'], // https://github.com/prettier/eslint-config-prettier#curly
-        'react/prop-types': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        '@typescript-eslint/no-unused-vars': ['warn'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+      project: './tsconfig.json',
+      ecmaFeatures: {
+        jsx: true,
       },
-    },  
+      warnOnUnsupportedTypeScriptVersion: true,
+  },
+  ignorePatterns: ['/dist', "/webpack/*.js", ".eslintrc.js"],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
   ],
+  plugins: ['import','react-hooks'],
+  rules: {
+    indent: ['warn', 2, { SwitchCase: 1 }],
+    'no-multi-spaces': ['warn'],
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    "prettier/prettier": ["warn"],
+    'import/order': ['error', importOrder],
+    'import/no-duplicates': 'error',
+    'curly': ['error', 'all'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
   settings: {
-    // Tells eslint-plugin-react to automatically detect the version of React to use
     react: { version: 'detect' },
   },
 };
